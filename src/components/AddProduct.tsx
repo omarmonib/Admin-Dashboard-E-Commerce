3b0d3cbf61c56d3398374688646fa0374970b663
Omar Monib <omar.monib.03@gmail.com>
2026-01-15 08:29:15 +0200

    push project

---
+++ src/components/AddProduct.tsx
@@ -0,0 +1,290 @@
+'use client';
+
+import { SheetContent, SheetDescription, SheetHeader, SheetTitle } from '@/components/ui/sheet';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from './ui/form';
+import { Input } from './ui/input';
+import { Button } from './ui/button';
+import { Textarea } from './ui/textarea';
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from './ui/select';
+import { Checkbox } from './ui/checkbox';
+import { ScrollArea } from './ui/scroll-area';
+
+const categories = [
+  'T-shirts',
+  'Shoes',
+  'Accessories',
+  'Bags',
+  'Dresses',
+  'Jackets',
+  'Gloves',
+] as const;
+
+const colors = [
+  'blue',
+  'green',
+  'red',
+  'yellow',
+  'purple',
+  'orange',
+  'pink',
+  'brown',
+  'gray',
+  'black',
+  'white',
+] as const;
+
+const sizes = [
+  'xs',
+  's',
+  'm',
+  'l',
+  'xl',
+  'xxl',
+  '34',
+  '35',
+  '36',
+  '37',
+  '38',
+  '39',
+  '40',
+  '41',
+  '42',
+  '43',
+  '44',
+  '45',
+  '46',
+  '47',
+  '48',
+] as const;
+
+const formSchema = z.object({
+  name: z.string().min(1, { message: 'Product name is required!' }),
+  shortDescription: z.string().min(1, { message: 'Short description is required!' }).max(60),
+  description: z.string().min(1, { message: 'Description is required!' }),
+  price: z.number().min(1, { message: 'Price is required!' }),
+  category: z.enum(categories),
+  sizes: z.array(z.enum(sizes)),
+  colors: z.array(z.enum(colors)),
+  images: z.record(z.enum(colors), z.string()),
+});
+
+const AddProduct = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: '',
+      shortDescription: '',
+      description: '',
+      price: 0,
+      category: undefined,
+      sizes: [],
+      colors: [],
+      images: {},
+    },
+  });
+  return (
+    <SheetContent>
+      <ScrollArea className="h-screen">
+        <SheetHeader>
+          <SheetTitle className="mb-4">Add Product</SheetTitle>
+          <SheetDescription asChild>
+            <Form {...form}>
+              <form className="space-y-8">
+                <FormField
+                  control={form.control}
+                  name="name"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Name</FormLabel>
+                      <FormControl>
+                        <Input {...field} />
+                      </FormControl>
+                      <FormDescription>Enter product name.</FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="shortDescription"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Short Description</FormLabel>
+                      <FormControl>
+                        <Input {...field} />
+                      </FormControl>
+                      <FormDescription> Enter product short description.</FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="description"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Description</FormLabel>
+                      <FormControl>
+                        <Textarea {...field} />
+                      </FormControl>
+                      <FormDescription>Enter product description.</FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="price"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Price</FormLabel>
+                      <FormControl>
+                        <Input type="number" {...field} />
+                      </FormControl>
+                      <FormDescription>Enter product price.</FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="category"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Category</FormLabel>
+                      <FormControl>
+                        <Select>
+                          <SelectTrigger>
+                            <SelectValue placeholder="Select a category" />
+                          </SelectTrigger>
+                          <SelectContent>
+                            {categories.map((category) => (
+                              <SelectItem key={category} value={category}>
+                                {category}
+                              </SelectItem>
+                            ))}
+                          </SelectContent>
+                        </Select>
+                      </FormControl>
+                      <FormDescription>Enter product category.</FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="sizes"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Sizes</FormLabel>
+                      <FormControl>
+                        <div className="grid grid-cols-3 gap-4 my-2">
+                          {sizes.map((size) => (
+                            <div key={size} className="flex items-center gap-2">
+                              <Checkbox
+                                id={size}
+                                checked={field.value.includes(size)}
+                                onCheckedChange={(checked) => {
+                                  const currentValues = field.value || [];
+                                  if (checked) {
+                                    field.onChange([...currentValues, size]);
+                                  } else {
+                                    field.onChange([
+                                      ...currentValues.filter(
+                                        (currentValues) => currentValues !== size
+                                      ),
+                                    ]);
+                                  }
+                                }}
+                              />
+                              <label htmlFor="size">{size}</label>
+                            </div>
+                          ))}
+                        </div>
+                      </FormControl>
+                      <FormDescription>Enter available product sizes.</FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="colors"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Colors</FormLabel>
+                      <FormControl>
+                        <div className="space-y-4">
+                          <div className="grid grid-cols-3 gap-4 my-2">
+                            {colors.map((color) => (
+                              <div key={color} className="flex items-center gap-2">
+                                <Checkbox
+                                  id={color}
+                                  checked={field.value?.includes(color)}
+                                  onCheckedChange={(checked) => {
+                                    const currentColors = field.value || [];
+                                    if (checked) {
+                                      field.onChange([...currentColors, color]);
+                                    } else {
+                                      field.onChange([
+                                        ...currentColors.filter(
+                                          (currentColors) => currentColors !== color
+                                        ),
+                                      ]);
+                                    }
+                                  }}
+                                />
+                                <label htmlFor="size" className="text-xs flex items-center gap-2">
+                                  <div
+                                    className="w-2 h-2 rounded-full"
+                                    style={{ backgroundColor: color }}
+                                  />
+                                  {color}
+                                </label>
+                              </div>
+                            ))}
+                          </div>
+                          {field.value && field.value.length > 0 && (
+                            <div className="mt-8 space-y-4">
+                              <p className="text-sm font-medium">Upload images for selected colors:</p>
+                              {field.value.map((color) => (
+                                <div className="flex items-center gap-2" key={color}>
+                                  <div
+                                    className="w-2 h-2 rounded-full"
+                                    style={{ backgroundColor: color }}
+                                  />
+                                  <span className='text-sm min-w-[60px]'>{color}</span>
+                                  <Input type="file" accept="image/*" />
+                                </div>
+                              ))}
+                            </div>
+                          )}
+                        </div>
+                      </FormControl>
+                      <FormDescription>Enter available product colors.</FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <Button type="submit">Submit</Button>
+              </form>
+            </Form>
+          </SheetDescription>
+        </SheetHeader>
+      </ScrollArea>
+    </SheetContent>
+  );
+};
+
+export default AddProduct;
